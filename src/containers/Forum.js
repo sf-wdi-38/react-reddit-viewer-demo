f1ed63df4f87d788cd1665ed5b11579e9a03faa0
nathanallen <thenathanator@gmail.com>
2017-06-13 11:51:39 -0700

    naming things is hard

--- src/containers/Forum.js
+++ src/containers/Forum.js
@@ -10,14 +10,14 @@ class Forum extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      threads: ["Damn Wish!", "I was walking..."]
+      thread_titles: ["Damn Wish!", "I was walking..."]
     }
   }
   render() {
     return (
         <div>
           <h1>The { this.props.params.forum_name } Subreddit</h1>
-          { this.state.threads.map((title) => <Thread title={title} />) }
+          { this.state.thread_titles.map((title) => <Thread title={title} />) }
         </div>
 
     );
