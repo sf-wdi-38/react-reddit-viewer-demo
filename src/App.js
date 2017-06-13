21f5970c90c7392c871c7e9f44480cbc3b7cbf14
nathanallen <thenathanator@gmail.com>
2017-06-13 11:00:58 -0700

    working forum links on homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,10 +12,10 @@ class App extends Component {
           <h2>Reddit Viewer</h2>
         </div>
         <ul>
-          <li>funny</li>
-          <li>reactjs</li>
-          <li>holdmybeer</li>
-          <li>gifsthatkeepongiving</li>
+          <li><Link to="funny">funny</Link></li>
+          <li><Link to="reactjs">reactjs</Link></li>
+          <li><Link to="holdmybeer">holdmybeer</Link></li>
+          <li><Link to="gifsthatkeepongiving">gifsthatkeepongiving</Link></li>
         </ul>
         { this.props.children }
 
