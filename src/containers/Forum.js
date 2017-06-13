0ceb1c193756e0e8e0d0e66983457e1078779321
nathanallen <thenathanator@gmail.com>
2017-06-13 11:40:48 -0700

    basic Thread component

--- src/containers/Forum.js
+++ src/containers/Forum.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+class Thread extends Component {
+  render() {
+    return (<p>{this.props.title}</p>);
+  }
+}
+
 class Forum extends Component {
   render() {
     let threads = ["Damn Wish!", "I was walking..."];
@@ -7,7 +13,7 @@ class Forum extends Component {
     return (
         <div>
           <h1>The { this.props.params.forum_name } Subreddit</h1>
-          { threads.map((title) => <p>{title}</p>) }
+          { threads.map((title) => <Thread title={title} />) }
         </div>
 
     );
